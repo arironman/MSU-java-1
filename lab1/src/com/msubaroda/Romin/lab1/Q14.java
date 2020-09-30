56c9dc5b997529605b436eb66628ca1d71d1288f
Anurag Rai <71434438+arironman@users.noreply.github.com>
2020-10-01 01:48:43 +0530

    Update Q14.java
    Add more functionalities to the file

--- lab1/src/com/msubaroda/Romin/lab1/Q14.java
+++ lab1/src/com/msubaroda/Romin/lab1/Q14.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 
 public class Q14 {
     public static void main(String[] args) {
+//         Creating objects of Scanner Class 
         Scanner sc = new Scanner(System.in);
+//         Taking input the principle amount
         System.out.print("Enter the Principle: ");
         int principle = sc.nextInt();
+//         Taking input the rate
         System.out.print("Enter the Rate: ");
         int rate = sc.nextInt();
+//         taking Input the Time
         System.out.print("Enter the Time: ");
         int time = sc.nextInt();
+//         Calculating the Simple Intrest
         float si = (principle * rate * time)/100;
         System.out.println("The simple intrest is: " + si);
 
